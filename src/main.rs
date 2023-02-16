2fade4d1006dc8c3c3b2f90691a1549c0c06b30d
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2023-02-16 14:48:46 -0300

    Added raylib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(1280, 720)
+        .title("Dialogue maker")
+        .build();
+     
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+         
+        d.clear_background(Color::WHITE);
+        d.draw_rectangle(0, 0, 1280, 720, Color::RED);
+        d.draw_rectangle(10, 10, 1260, 700, Color::WHITE);
+        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+    }
 }
