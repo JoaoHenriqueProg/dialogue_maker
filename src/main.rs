64d003ebfa5c1454cb0574a99e89f77819d72c79
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2023-02-16 16:46:48 -0300

    Implemented zoom and made some math to find the corner points in world

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,30 @@ fn main() {
 
     let mut cam = Camera2D::default();
     cam.zoom = 1.;
+    cam.offset = Vector2{x: 1280./2., y: 720./2.};
 
     // Raylib in rust for some reason doesn't provide a get_mouse_delta funcion, so the program will do it ny itself
     let mut last_mouse_pos = rl.get_mouse_position();
 
     while !rl.window_should_close() {
         // ===== UPDATE =====
-        
+
         // Adapted from 2d camera_mouse_zoom found at: https://www.raylib.com/examples.html
         if rl.is_mouse_button_down(MouseButton::MOUSE_RIGHT_BUTTON) {
             let mut delta = rl.get_mouse_position() - last_mouse_pos;
-            delta.scale(-1./cam.zoom);
+            delta.scale(-1. / cam.zoom);
 
             cam.target = cam.target + delta;
         }
         last_mouse_pos = rl.get_mouse_position();
-        
-        
-        
+
+        let wheel = rl.get_mouse_wheel_move();
+        if wheel != 0. {
+            let zoom_increment = 0.125;
+
+            cam.zoom += wheel * zoom_increment;
+        }
+
         // ===== DRAW =====
 
         let mut d = rl.begin_drawing(&thread);
@@ -35,8 +41,19 @@ fn main() {
         let mut new_d = d.begin_mode2D(cam);
 
         new_d.clear_background(Color::WHITE);
-        new_d.draw_rectangle(0, 0, 1280, 720, Color::RED);
-        new_d.draw_rectangle(10, 10, 1260, 700, Color::WHITE);
+
+        let tlp = cam.target - Vector2{x: 1280./2., y: 720./2.} * (1./cam.zoom);
+        let trp = cam.target + Vector2 { x: 1280., y: 0. } * (1./cam.zoom) - Vector2{x: 1280./2., y: 720./2.} * (1./cam.zoom);
+        let blp = cam.target + Vector2 { x: 0., y: 720. } * (1./cam.zoom) - Vector2{x: 1280./2., y: 720./2.} * (1./cam.zoom);
+        let brp = cam.target + Vector2 { x: 1280., y: 720. } * (1./cam.zoom) - Vector2{x: 1280./2., y: 720./2.} * (1./cam.zoom);
+
+        new_d.draw_circle(tlp.x as i32, tlp.y as i32, 5., Color::GREEN);
+        new_d.draw_circle(trp.x as i32, trp.y as i32, 5., Color::PINK);
+        new_d.draw_circle(blp.x as i32, blp.y as i32, 5., Color::BLUE);
+        new_d.draw_circle(brp.x as i32, brp.y as i32, 5., Color::RED);
+
+        //new_d.draw_rectangle(0, 0, 1280, 720, Color::RED);
+        //new_d.draw_rectangle(10, 10, 1260, 700, Color::WHITE);
         new_d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
     }
 }
