aeb499fd064d5fc34d321e4068388f893194b935
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2023-02-16 21:18:26 -0300

    Made a draw_canvas_background function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
 use raylib::prelude::*;
 
+fn draw_canvas_background(
+    d: &mut RaylibMode2D<'_, RaylibDrawHandle>,
+    top_left: Vector2,
+    bottom_right: Vector2,
+) {
+    for i in (top_left.x - 50.) as i32 / 50..(bottom_right.x + 50.) as i32 / 50 {
+        d.draw_line(
+            i * 50,
+            top_left.y as i32,
+            i * 50,
+            bottom_right.y as i32,
+            Color::BLACK,
+        );
+    }
+    for i in (top_left.y - 50.) as i32 / 50..(bottom_right.y + 50.) as i32 / 50 {
+        d.draw_line(
+            top_left.x as i32,
+            i * 50,
+            bottom_right.x as i32,
+            i * 50,
+            Color::BLACK,
+        );
+    }
+
+    d.draw_line(0, i32::MIN, 0, i32::MAX, Color::ORANGE);
+    d.draw_line(i32::MIN, 0, i32::MAX, 0, Color::ORANGE);
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(1280, 720)
@@ -47,15 +75,18 @@ fn main() {
 
         let mut new_d = d.begin_mode2D(cam);
 
+        draw_canvas_background(&mut new_d, tlp.clone(), brp.clone());
+
         new_d.clear_background(Color::WHITE);
 
-        new_d.draw_circle(tlp.x as i32, tlp.y as i32, 5., Color::GREEN);
-        new_d.draw_circle(trp.x as i32, trp.y as i32, 5., Color::PINK);
-        new_d.draw_circle(blp.x as i32, blp.y as i32, 5., Color::BLUE);
-        new_d.draw_circle(brp.x as i32, brp.y as i32, 5., Color::RED);
+        // new_d.draw_circle(tlp.x as i32, tlp.y as i32, 5., Color::GREEN);
+        // new_d.draw_circle(trp.x as i32, trp.y as i32, 5., Color::PINK);
+        // new_d.draw_circle(blp.x as i32, blp.y as i32, 5., Color::BLUE);
+        // new_d.draw_circle(brp.x as i32, brp.y as i32, 5., Color::RED);
 
         //new_d.draw_rectangle(0, 0, 1280, 720, Color::RED);
         //new_d.draw_rectangle(10, 10, 1260, 700, Color::WHITE);
         new_d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        new_d.draw_fps(tlp.x as i32, tlp.y as i32);
     }
 }
