17813881853ce8c49f83df3aa6ab1c5463825d59
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2023-02-18 15:42:40 -0300

    I lost track of myself and programmed a lot without commiting, so here's what I remember I've done: base of node pool, parsing node pool into canvas cards and drawing canvas cards together with their widgets. I went totally free style, this may be the worst code I've ever written in my entire life.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,138 @@
 use raylib::prelude::*;
 
-trait CanvasCard {
-    fn set_pos(&mut self, new_pos: Vector2);
-    fn get_pos(&self) -> Vector2;
-    fn set_size(&mut self, new_size: Vector2);
-    fn get_size(&self) -> Vector2;
+enum NodeTypes {
+    Dialogue,
+    Options,
+    Conditional,
+    SetFlag,
+    SomethingHasGoneReallyWrong,
+}
+
+impl Default for NodeTypes {
+    fn default() -> NodeTypes {
+        NodeTypes::SomethingHasGoneReallyWrong
+    }
+}
+
+#[derive(Default)]
+struct Node {
+    character: Option<String>,
+    dialogue: Option<String>,
+    options: Option<Vec<String>>,
+    flag_to_check: Option<String>,
+    flag_to_set: Option<String>,
+    value_to_set: Option<String>,
+    node_type: NodeTypes,
+}
+
+impl Node {
+    fn default_dialogue() -> Node {
+        Node {
+            character: Some("".to_string()),
+            dialogue: Some("".to_string()),
+            options: None,
+            flag_to_check: None,
+            flag_to_set: None,
+            value_to_set: None,
+            node_type: NodeTypes::Dialogue,
+        }
+    }
+
+    fn new_dialogue<T: ToString>(character: T, dialogue: T) -> Node {
+        let mut to_return = Node::default_dialogue();
+        to_return.character = Some(character.to_string());
+        to_return.dialogue = Some(dialogue.to_string());
+
+        to_return
+    }
+}
+
+// Note: Cards and widgets will be references to nodes, nodes will not have access to anything related to cards and widgets, but cards and widgets will have knowledge of nodes
+
+enum WidgetType {
+    TextInput,
+}
+
+struct Widget {
+    value: String,
+    widget_type: WidgetType,
+}
+
+impl Widget {
+    fn set_value(&mut self, new_val: String) {
+        self.value = new_val;
+    }
+    fn get_val(&self) -> String {
+        self.value.clone()
+    }
+
+    fn draw(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>, in_world_origin_pos: Vector2) {
+        match self.widget_type {
+            WidgetType::TextInput => {
+                d.draw_rectangle(
+                    in_world_origin_pos.x as i32,
+                    in_world_origin_pos.y as i32,
+                    150,
+                    25,
+                    Color::GRAY,
+                );
+                d.draw_rectangle(
+                    in_world_origin_pos.x as i32 + 1,
+                    in_world_origin_pos.y as i32 + 1,
+                    148,
+                    23,
+                    Color::WHITE,
+                );
+                d.draw_text(
+                    &self.value,
+                    in_world_origin_pos.x as i32 + 3,
+                    in_world_origin_pos.y as i32 + 3,
+                    19,
+                    Color::BLACK,
+                )
+            }
+        }
+    }
+
+    fn was_clicked(&self, in_world_origin_pos: Vector2, in_world_mouse_pos: Vector2) -> bool {
+        let size = match self.widget_type {
+            WidgetType::TextInput => Vector2 { x: 150., y: 25. },
+        };
+
+        let origin_x = in_world_origin_pos.x as i32;
+        let origin_y = in_world_origin_pos.y as i32;
+        let mouse_x = in_world_mouse_pos.x as i32;
+        let mouse_y = in_world_mouse_pos.y as i32;
+
+        if mouse_x > origin_x && mouse_x < origin_x + size.x as i32 {
+            if mouse_y > origin_y && mouse_y < origin_y + size.y as i32 {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+struct Card {
+    pos: Vector2,
+    size: Vector2,
+    widgets: Vec<Widget>,
+    card_type: NodeTypes,
+}
+
+impl Card {
+    fn set_pos(&mut self, new_pos: Vector2) {
+        self.pos = new_pos;
+    }
+    fn get_pos(&self) -> Vector2 {
+        self.pos
+    }
+    fn set_size(&mut self, new_size: Vector2) {
+        self.size = new_size;
+    }
+    fn get_size(&self) -> Vector2 {
+        self.size
+    }
     fn update(
         &mut self,
         rl: &RaylibHandle,
@@ -36,10 +164,66 @@ trait CanvasCard {
     }
 
     fn draw(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>) {
-        self.draw_node_bg(d);
+        self.draw_card_bg(d);
+        match self.card_type {
+            NodeTypes::Dialogue => {
+                let mut y_offset = 0.;
+                y_offset += 10.;
+
+                self.draw_lable(
+                    d,
+                    "Character:",
+                    Vector2 {
+                        x: 10.,
+                        y: y_offset,
+                    },
+                );
+                y_offset += 25.;
+                y_offset += 10.;
+
+                let chr_label = &self.widgets[0];
+                chr_label.draw(
+                    d,
+                    self.pos
+                        + Vector2 {
+                            x: 10.,
+                            y: y_offset,
+                        },
+                );
+                y_offset += 25.;
+                y_offset += 10.;
+
+                self.draw_lable(
+                    d,
+                    "Dialogue:",
+                    Vector2 {
+                        x: 10.,
+                        y: y_offset,
+                    },
+                );
+                y_offset += 25.;
+                y_offset += 10.;
+
+                let dlg_label = &self.widgets[1];
+                dlg_label.draw(d, self.pos + Vector2 { x: 10., y: y_offset });
+                
+                println!("{}", y_offset);
+            }
+            _ => unimplemented!(),
+        }
     }
 
-    fn draw_node_bg(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>) {
+    fn draw_lable(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>, text: &str, offset: Vector2) {
+        d.draw_text(
+            text,
+            (self.pos + offset).x as i32,
+            (self.pos + offset).y as i32,
+            24,
+            Color::BLACK,
+        );
+    }
+
+    fn draw_card_bg(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>) {
         let corner_radius = 10;
 
         d.draw_circle(
@@ -106,88 +290,15 @@ trait CanvasCard {
     }
 }
 
-struct DialogueCard {
-    pos: Vector2,
-    size: Vector2,
-}
-struct OptionsCard {
-    pos: Vector2,
-    size: Vector2,
-}
-struct ConditionalCard {
-    pos: Vector2,
-    size: Vector2,
-}
-struct SetFlagCard {
-    pos: Vector2,
-    size: Vector2,
-}
-
-impl CanvasCard for DialogueCard {
-    fn set_pos(&mut self, new_pos: Vector2) {
-        self.pos = new_pos;
-    }
-    fn get_pos(&self) -> Vector2 {
-        self.pos
-    }
-    fn set_size(&mut self, new_size: Vector2) {
-        self.size = new_size;
-    }
-    fn get_size(&self) -> Vector2 {
-        self.size
-    }
-}
-impl CanvasCard for OptionsCard {
-    fn set_pos(&mut self, new_pos: Vector2) {
-        self.pos = new_pos;
-    }
-    fn get_pos(&self) -> Vector2 {
-        self.pos
-    }
-    fn set_size(&mut self, new_size: Vector2) {
-        self.size = new_size;
-    }
-    fn get_size(&self) -> Vector2 {
-        self.size
-    }
-}
-impl CanvasCard for ConditionalCard {
-    fn set_pos(&mut self, new_pos: Vector2) {
-        self.pos = new_pos;
-    }
-    fn get_pos(&self) -> Vector2 {
-        self.pos
-    }
-    fn set_size(&mut self, new_size: Vector2) {
-        self.size = new_size;
-    }
-    fn get_size(&self) -> Vector2 {
-        self.size
-    }
-}
-impl CanvasCard for SetFlagCard {
-    fn set_pos(&mut self, new_pos: Vector2) {
-        self.pos = new_pos;
-    }
-    fn get_pos(&self) -> Vector2 {
-        self.pos
-    }
-    fn set_size(&mut self, new_size: Vector2) {
-        self.size = new_size;
-    }
-    fn get_size(&self) -> Vector2 {
-        self.size
-    }
-}
-
 struct CanvasScene {
     cam: Camera2D,
-    nodes: Vec<Box<dyn CanvasCard>>,
+    cards: Vec<Card>,
+    node_pool: Vec<Node>,
 }
 
 impl CanvasScene {
     pub fn update(&mut self, rl: &RaylibHandle, last_mouse_pos: &mut Vector2) {
-        for i in self.nodes.iter_mut() {
+        for i in self.cards.iter_mut() {
             i.update(rl, last_mouse_pos, self.cam.zoom, &self.cam);
         }
 
@@ -238,8 +349,45 @@ impl CanvasScene {
     }
 
     pub fn draw(&self, d: &mut RaylibMode2D<'_, RaylibDrawHandle>) {
-        for i in &self.nodes {
+        for i in &self.cards {
             i.draw(d);
+        }
+    }
+
+    pub fn parse_node_pool(&mut self) {
+        println!("Parsing node_pool");
+
+        for i in &self.node_pool {
+            match i.node_type {
+                NodeTypes::Dialogue => {
+                    let chr = i
+                        .character
+                        .clone()
+                        .unwrap_or("ERROR: NO CHARACTER FOUND".to_string());
+                    let dlg = i
+                        .dialogue
+                        .clone()
+                        .unwrap_or("ERROR: NO CHARACTER FOUND".to_string());
+
+                    self.cards.push(Card {
+                        pos: Vector2::default(),
+                        size: Vector2 { x: 170., y: 150. },
+                        widgets: vec![
+                            Widget {
+                                value: chr,
+                                widget_type: WidgetType::TextInput,
+                            },
+                            Widget {
+                                value: dlg,
+                                widget_type: WidgetType::TextInput,
+                            },
+                        ],
+                        card_type: NodeTypes::Dialogue,
+                    });
+                }
+
+                _ => unimplemented!(),
+            }
         }
     }
 }
@@ -262,11 +410,10 @@ fn main() {
             target: Vector2::default(),
             rotation: 0.,
         },
-        nodes: vec![Box::new(DialogueCard {
-            pos: Vector2 { x: 800., y: 200. },
-            size: Vector2 { x: 600., y: 400. },
-        })],
+        cards: Vec::default(),
+        node_pool: vec![Node::new_dialogue("John doe", "Test test testing")],
     };
+    canvas_scene.parse_node_pool();
 
     // Raylib in rust for some reason doesn't provide a get_mouse_delta funcion, so the program will do it ny itself
     let mut last_mouse_pos = rl.get_mouse_position();
@@ -289,7 +436,7 @@ fn main() {
         canvas_scene.draw_background(&mut new_d, tlp.clone(), brp.clone());
         canvas_scene.draw(&mut new_d);
 
-        new_d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        // new_d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
         new_d.draw_fps(tlp.x as i32, tlp.y as i32);
     }
 }
